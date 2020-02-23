e8cb5c90cb9ace27a1bd871a9f893207a5e51eb8
Patrick Gibson <pg@pagibson.com>
2020-02-23 18:37:47 -0500

    add generation function

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import itertools
 from argparse import ArgumentParser
+
+import optimize
 
 def play(filename=None):
     if filename:
         print("Filename supplied: {}".format(filename))
     print("Oh boy, playing the game!")
 
+def _print_found_word(wordList):
+    print("{} {}".format(wordList.pivot_letter, wordList.letters))
+
+def _save_found_word(filename, wordList):
+    with open(filename, 'a') as f:
+        f.write("({},{})\n".format(wordList.pivot_letter, wordList.letters))
+
 def generate(filename=None):
-    if filename:
-        print("Filename supplied: {}".format(filename))
-    print("Gee willikers, generating some words!")
+    if filename is None:
+        filename = "pangrams.txt"
+    try:
+        for i in itertools.count():
+            if i%10 == 0:
+                print("You can exit any time with Ctrl-C.")
+            wordList = optimize.find_word()
+            _print_found_word(wordList)
+            _save_found_word(filename, wordList)
+    except KeyboardInterrupt:
+        return
 
 def main():
     parser = ArgumentParser("Play SpellingBean and generate new " +
@@ -16,7 +34,9 @@ def main():
     parser.add_argument("action",
                         choices=["play", "generate"])
     parser.add_argument("-f", "--file")
+
     a = parser.parse_args()
+
     if a.action == "play":
         play(a.file)
     else:
